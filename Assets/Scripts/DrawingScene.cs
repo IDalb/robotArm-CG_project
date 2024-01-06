eaeb51260457c569c1100f2ad8208c144ee5818d
Tartiine <sofia.saadi6@gmail.com>
2024-01-06 15:54:05 +0100

    First version of "ensisa" drawing

---
+++ Assets/Scripts/DrawingScene.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using UnityEngine;
+
+public class DrawingScene : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Light spotlightLeft;
+    public Light spotlightRight; 
+    public AudioSource soundEffect; 
+    public AudioClip spotlightSound; 
+    public AudioClip drawingSound; 
+    public InverseKinematics inverseKinematics;
+
+    private void Start()
+    {
+        StartCoroutine(StartDrawingSequence());
+    }
+
+    IEnumerator StartDrawingSequence()
+    {
+        PositionAndOrientCameraAndSpotlights();
+        soundEffect.PlayOneShot(spotlightSound);
+        StartCoroutine(FadeInLight(spotlightLeft, 5f)); 
+        StartCoroutine(FadeInLight(spotlightRight, 5f)); 
+        yield return new WaitForSeconds(spotlightSound.length);
+
+        // TODO:Must be looped while drawing
+        soundEffect.PlayOneShot(drawingSound);
+        inverseKinematics.StartDrawing();
+    }
+
+    void PositionAndOrientCameraAndSpotlights()
+    {
+        // Camera setup
+        mainCamera.transform.position = new Vector3(0, 5, -10);
+        mainCamera.transform.LookAt(new Vector3(0, 0, 0));
+
+        // Spotlight setup
+        spotlightLeft.transform.position = new Vector3(-5, 5, -5);
+        spotlightRight.transform.position = new Vector3(5, 5, -5);
+        spotlightLeft.transform.LookAt(new Vector3(0, 0, 0));
+        spotlightRight.transform.LookAt(new Vector3(0, 0, 0));
+    }
+
+
+    IEnumerator FadeInLight(Light light, float duration)
+    {
+        float time = 0;
+        float initialIntensity = light.intensity;
+        while (time < duration)
+        {
+            light.intensity = Mathf.Lerp(0, initialIntensity, time / duration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+    }
+}
+
